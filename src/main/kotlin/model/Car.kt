3c6a70433b602ab7e3b1464be8ffa2d37cadb91f
hanbee son <hbson@hyundai.com>
2025-04-20 17:17:41 +0900

    feat: Car 객체 구현

--- src/main/kotlin/model/Car.kt
+++ src/main/kotlin/model/Car.kt
@@ -1,4 +1,17 @@
 package model
 
-class Car {
+class Car(
+    val name: String,
+    var distance: Int = 0,
+) {
+    init {
+        require(name.length <= 5) { "[ERROR] 자동차 이름은 5글자 이하여야 합니다." }
+    }
+
+    fun move() {
+        val randomValue = (0..9).random()
+        if (randomValue >= 4) {
+            this.distance += randomValue
+        }
+    }
 }
